eb76d256601d5c49e0e7b523b9bc7219c2f6d0e3
Greg <gregorypratt@users.noreply.github.com>
2015-02-24 15:14:05 +0000

    Removed extra User require

--- app.js
+++ app.js
@@ -59,7 +59,6 @@ app.use(function (err, req, res) {
 });
 
 // User and Passport config
-var User = require('./models/user');
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
